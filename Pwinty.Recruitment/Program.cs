b2b85db6e4681f658086c8cf9ae44a2ae7288528
Tom Gallard <tom.gallard@cantab.net>
2019-01-14 20:37:19 +0000

    Add project files.

---
+++ Pwinty.Recruitment/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Pwinty.Recruitment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
